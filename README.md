fdf32adb6ba7e2b53b551882a7dcd8b23d5a340d
Calderoni <marcelo@transvectologistics.com>
2023-09-03 18:05:47 -0600

    Finished product

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Server-API-Challenge-6
+
+Your Weather Channel
+A simple weather application to get the current weather, and a 5-day forecast for any city.
+
+Features:
+Search for a city
+Get current temperature, wind speed, and humidity
+Get a 5-day forecast including temperature, wind speed, and humidity
+Save the last 5 searched cities
+
+Usage:
+Open index.html in a browser.
+Enter a city name in the search box and click 'Search'.
+The current weather and 5-day forecast will be displayed.
+
+Technologies Used:
+HTML
+CSS
+JavaScript
+jQuery
+OpenWeatherMap API
+
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,114 +1,164 @@
-
 $(document).ready(function() {
 
-    $('button').click(function() {
-        var cityName = $('input[type="text"]').val();
-        var apiKey = '7b300d672c57ffad22d9822779ecd22c';
-            $.ajax({
-                url: 'http://api.openweathermap.org/geo/1.0/direct',
-                method: 'GET',
-                data: {
-                    q: cityName,
-                    limit: 1,
-                    appid: apiKey
-                },
-                success: function(responseCoordinates) {
-                    var latitude = responseCoordinates[0].lat;
-                    var longitude = responseCoordinates[0].lon;
-                    console.log('Latitude: ' + latitude + ', Longitude: ' + longitude);
-
-                    $.ajax({
-                        url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`,
-                        method: 'GET',
-                        success: function(responseWeather) {
-                            console.log(responseWeather);
-                            var temp = responseWeather.main.temp;
-                            var wind = responseWeather.wind.speed;
-                            var humidity = responseWeather.main.humidity;
-                            console.log('temp:' + temp + 'wind:' + wind + 'humidity:' + humidity);
-
-                            document.getElementById('city').textContent = cityName;
-                            document.getElementById('temp').textContent = 'Temperature: ' + temp;
-                            document.getElementById('wind').textContent = 'Wind: ' + wind;
-                            document.getElementById('humidity').textContent = 'Humidity: ' + humidity;
-
-                            $.ajax({
-                                url: `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}&cnt=40`,
-                                method: 'GET',
-                                success: function(weatherForecast) {
-                                    var dayOne = weatherForecast.list[3];
-                                    var dayTwo = weatherForecast.list[11];
-                                    var dayThree = weatherForecast.list[19];
-                                    var dayFour = weatherForecast.list[27];
-                                    var dayFive = weatherForecast.list[35];
-
-                                    var datePartOne = dayOne.dt_txt.split(' ')[0];
-                                    var datePartTwo = dayTwo.dt_txt.split(' ')[0];
-                                    var datePartThree = dayThree.dt_txt.split(' ')[0];
-                                    var datePartFour = dayFour.dt_txt.split(' ')[0];
-                                    var datePartFive = dayFive.dt_txt.split(' ')[0];
-
-                                    document.getElementById('dayOneDate').textContent = datePartOne;
-                                    document.getElementById('dayOneTemp').textContent = 'Temperature: ' + dayOne.main.temp;
-                                    document.getElementById('dayOneWind').textContent = 'Wind Speed: ' + dayOne.wind.speed;
-                                    document.getElementById('dayOneHumidity').textContent = 'Humidity: ' + dayOne.main.humidity;
-
-                                    document.getElementById('dayTwoDate').textContent = datePartTwo;
-                                    document.getElementById('dayTwoTemp').textContent = 'Temperature: ' + dayTwo.main.temp;
-                                    document.getElementById('dayTwoWind').textContent = 'Wind Speed: ' + dayTwo.wind.speed;
-                                    document.getElementById('dayTwoHumidity').textContent = 'Humidity: ' + dayTwo.main.humidity;
-
-                                    document.getElementById('dayThreeDate').textContent = datePartThree;
-                                    document.getElementById('dayThreeTemp').textContent = 'Temperature: ' + dayThree.main.temp;
-                                    document.getElementById('dayThreeWind').textContent = 'Wind Speed: ' + dayThree.wind.speed;
-                                    document.getElementById('dayThreeHumidity').textContent = 'Humidity: ' + dayThree.main.humidity;
-
-                                    document.getElementById('dayFourDate').textContent = datePartFour;
-                                    document.getElementById('dayFourTemp').textContent = 'Temperature: ' + dayFour.main.temp;
-                                    document.getElementById('dayFourWind').textContent = 'Wind Speed: ' + dayFour.wind.speed;
-                                    document.getElementById('dayFourHumidity').textContent = 'Humidity: ' + dayFour.main.humidity;
-
-                                    document.getElementById('dayFiveDate').textContent = datePartFive;
-                                    document.getElementById('dayFiveTemp').textContent = 'Temperature: ' + dayFive.main.temp;
-                                    document.getElementById('dayFiveWind').textContent = 'Wind Speed: ' + dayFive.wind.speed;
-                                    document.getElementById('dayFiveHumidity').textContent = 'Humidity: ' + dayFive.main.humidity;
-
-                                    $("#dayOneDate, #dayOneTemp, #dayOneWind, #dayOneHumidity").parent().show();
-                                    $("#dayTwoDate, #dayTwoTemp, #dayTwoWind, #dayTwoHumidity").parent().show();
-                                    $("#dayThreeDate, #dayThreeTemp, #dayThreeWind, #dayThreeHumidity").parent().show();
-                                    $("#dayFourDate, #dayFourTemp, #dayFourWind, #dayFourHumidity").parent().show();
-                                    $("#dayFiveDate, #dayFiveTemp, #dayFiveWind, #dayFiveHumidity").parent().show();
-                                },
-                                error: function(error) {
-                                    console.log(error);
-                                }
-                            });
-
-                        },
-                        error: function(error) {
-                            console.log(error);
-                        }
-                    });
-                
-                },
-                error: function(error) {
-                    console.log(error);
-                }
+    function updateSearchButtons() {
+        var searches = JSON.parse(localStorage.getItem('searches')) || [];
+        var savedCitiesContainer = $('#savedCitiesContainer');
+        savedCitiesContainer.empty();
+        searches.forEach(function(search) {
+            var button = $('<button>').text(search);
+            button.click(function() {
+                $('input[type="text"]').val(search);
+                $('button').click();
             });
+            savedCitiesContainer.append(button);
+        });
+    }
+    updateSearchButtons();
 
-           
-        let savedCities = JSON.parse(localStorage.getItem('savedCity')) || [];
-        if (!savedCities.includes(cityName)) {
-            savedCities.push(cityName);
-            localStorage.setItem('savedCity', JSON.stringify(savedCities));
-        }
-        populateSavedCities(savedCities);
 
-        updateSavedCities(cityName);
-        populateSavedCitiesButtons();
+    // When the button is clicked
+    $('button').click(function() {
+        // Get the entered city name
+        var cityName = $('input[type="text"]').val();
+
+    if (cityName) {
+        var searches = JSON.parse(localStorage.getItem('searches')) || [];
+        // Add the new search to the beginning of the array
+        searches.unshift(cityName);
+        // Remove duplicates
+        searches = Array.from(new Set(searches));
+        // Keep only the last 5 searches
+        searches = searches.slice(0, 5);
+        // Save the updated searches to local storage
+        localStorage.setItem('searches', JSON.stringify(searches));
+
+        updateSearchButtons();
+    }
+
+        var apiKey = '7b300d672c57ffad22d9822779ecd22c';
+
+        // Make an AJAX call to get coordinates of the entered city
+        $.ajax({
+            url: 'http://api.openweathermap.org/geo/1.0/direct',
+            method: 'GET',
+            data: {
+                q: cityName,
+                limit: 1,
+                appid: apiKey
+            },
+            success: function(responseCoordinates) {
+                var latitude = responseCoordinates[0].lat;
+                var longitude = responseCoordinates[0].lon;
+                console.log('Latitude: ' + latitude + ', Longitude: ' + longitude);
+
+                // Make another AJAX call to get current weather details using the coordinates
+                $.ajax({
+                    url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`,
+                    method: 'GET',
+                    success: function(responseWeather) {
+                        console.log(responseWeather);
+                        var temp = responseWeather.main.temp;
+                        var wind = responseWeather.wind.speed;
+                        var humidity = responseWeather.main.humidity;
+                        console.log('temp:' + temp + 'wind:' + wind + 'humidity:' + humidity);
+
+                        // Set the weather details to respective elements
+                        document.getElementById('city').textContent = cityName;
+                        document.getElementById('temp').textContent = 'Temperature: ' + temp;
+                        document.getElementById('wind').textContent = 'Wind: ' + wind;
+                        document.getElementById('humidity').textContent = 'Humidity: ' + humidity;
+
+                        // Make a third AJAX call to get a 5-day forecast using the coordinates
+                        $.ajax({
+                            url: `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}&cnt=40`,
+                            method: 'GET',
+                            success: function(weatherForecast) {
+                                // Extract data for each of the 5 days and set to elements
+                                var dayOne = weatherForecast.list[3];
+                                var dayTwo = weatherForecast.list[11];
+                                var dayThree = weatherForecast.list[19];
+                                var dayFour = weatherForecast.list[27];
+                                var dayFive = weatherForecast.list[35];
+
+                                // Indicators extraction Day 1
+                                var datePartOne = dayOne.dt_txt.split(' ')[0];
+                                var tempPartOne = dayOne.main.temp;
+                                var windPartOne = dayOne.wind.speed;
+                                var humidityPartOne = dayOne.main.humidity;
+
+                                 // Indicators extraction Day 2
+                                 var datePartTwo = dayTwo.dt_txt.split(' ')[0];
+                                 var tempPartTwo = dayTwo.main.temp;
+                                 var windPartTwo = dayTwo.wind.speed;
+                                 var humidityPartTwo = dayTwo.main.humidity;
+
+                                  // Indicators extraction Day 3
+                                var datePartThree = dayThree.dt_txt.split(' ')[0];
+                                var tempPartThree = dayThree.main.temp;
+                                var windPartThree = dayThree.wind.speed;
+                                var humidityPartThree = dayThree.main.humidity;
+
+                                 // Indicators extraction Day 4
+                                 var datePartFour = dayFour.dt_txt.split(' ')[0];
+                                 var tempPartFour = dayFour.main.temp;
+                                 var windPartFour = dayFour.wind.speed;
+                                 var humidityPartFour = dayFour.main.humidity;
+
+                                  // Indicators extraction Day 5
+                                var datePartFive = dayFive.dt_txt.split(' ')[0];
+                                var tempPartFive = dayFive.main.temp;
+                                var windPartFive = dayFive.wind.speed;
+                                var humidityPartFive = dayFive.main.humidity;
+
+
+                                // Set the forecast details to respective elements for each day
+                                document.getElementById('dayOneDate').textContent = 'Date: ' + datePartOne;
+                                document.getElementById('dayOneTemp').textContent = 'Temperature: ' + tempPartOne + ' C';
+                                document.getElementById('dayOneWind').textContent = 'Wind Speed: ' + windPartOne;
+                                document.getElementById('dayOneHumidity').textContent = 'Humidity: ' + humidityPartOne;
+
+                                document.getElementById('dayTwoDate').textContent = 'Date: ' + datePartTwo;
+                                document.getElementById('dayTwoTemp').textContent = 'Temperature: ' + tempPartTwo + ' C';
+                                document.getElementById('dayTwoWind').textContent = 'Wind Speed: ' + windPartTwo;
+                                document.getElementById('dayTwoHumidity').textContent = 'Humidity: ' + humidityPartTwo;
+
+                                document.getElementById('dayThreeDate').textContent = 'Date: ' + datePartThree;
+                                document.getElementById('dayThreeTemp').textContent = 'Temperature: ' + tempPartThree + ' C';
+                                document.getElementById('dayThreeWind').textContent = 'Wind: ' + windPartThree;
+                                document.getElementById('dayThreeHumidity').textContent = 'Hunidity: ' + humidityPartThree;
+
+                                document.getElementById('dayFourDate').textContent = 'Date: ' + datePartFour;
+                                document.getElementById('dayFourTemp').textContent = 'Temperature: ' + tempPartFour + ' C';
+                                document.getElementById('dayFourWind').textContent = 'Wind Speed: ' + windPartFour;
+                                document.getElementById('dayFourHumidity').textContent = 'Humidity: ' + humidityPartFour;
+
+                                document.getElementById('dayFiveDate').textContent = 'Date: ' + datePartFive;
+                                document.getElementById('dayFiveTemp').textContent = 'Temperature: ' + tempPartFive + ' C';
+                                document.getElementById('dayFiveWind').textContent = 'Wind: ' + windPartFive;
+                                document.getElementById('dayFiveHumidity').textContent = 'Humidity: + ' + humidityPartFive;
+
+
+                                // Display the forecast elements
+                                $("#dayOneDate, #dayOneTemp, #dayOneWind, #dayOneHumidity").parent().show();
+                                $("#dayTwoDate, #dayTwoTemp, #dayTwoWind, #dayTwoHumidity").parent().show();
+                                $("#dayThreeDate, #dayThreeTemp, #dayThreeWind, #dayThreeHumidity").parent().show();
+                                $("#dayFourDate, #dayFourTemp, #dayFourWind, #dayFourHumidity").parent().show();
+                                $("#dayFiveDate, #dayFiveTemp, #dayFiveWind, #dayFiveHumidity").parent().show();
+                            },
+                            error: function(error) {
+                                console.log(error);
+                            }
+                        });
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+                
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     });
-
-    let initialSavedCities = JSON.parse(localStorage.getItem('savedCity')) || [];
-    populateSavedCities(initialSavedCities);
-
-});
+})
